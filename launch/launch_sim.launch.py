cf5c42b173afbaa20cec70f1689dcbae451ade20
Malvinhaparimwi <malvinhaparimwi@gmail.com>
2024-05-04 14:35:33 +0200

    make to match our robot

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,9 +2,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
+from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -18,6 +18,15 @@ def generate_launch_description():
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='holonomic' #<--- CHANGE ME
+    klt_world = 'worlds/world2.world'
+    world_path = get_package_share_directory(package_name, klt_world)
+    world = LaunchConfiguration('world')
+
+    declare_world_cmd = DeclareLaunchArgument(
+        name='world',
+        default_value=world_path,
+        description='Full path to the world model file to load'
+    )
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -29,6 +38,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'world': world}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -37,11 +47,10 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-
-
     # Launch them all!
     return LaunchDescription([
+        declare_world_cmd,
         rsp,
         gazebo,
-        spawn_entity,
+        spawn_entity
     ])
--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -20,6 +20,7 @@ def generate_launch_description():
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
+    
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
@@ -28,8 +29,7 @@ def generate_launch_description():
         output='screen',
         parameters=[params]
     )
-
-
+    
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -37,5 +37,5 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
     ])
